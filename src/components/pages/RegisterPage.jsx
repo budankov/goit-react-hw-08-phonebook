5240acddf54134f57e27f3b24695de74bab3d549
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-03-09 22:10:15 +0200

    Update

--- src/components/pages/RegisterPage.jsx
+++ src/components/pages/RegisterPage.jsx
@@ -1,5 +1,22 @@
+import styles from './styles.module.scss';
+
 const RegisterPage = () => {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerBcg}>
+        <div className={styles.contactBook}>
+          <h1 className={styles.title}>Phonebook</h1>
+          <div className={styles.mainContainer}>
+            <h2 className={styles.subTitle}>Welcome to the Phonebook</h2>
+            <p className={styles.subTitle}>
+              Dear User, in order to use the application, you need to Register
+              or LogIn if you already have an account.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;

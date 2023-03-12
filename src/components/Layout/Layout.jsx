96a6226f2c408a1132a576c84a6e93840dd3db47
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-03-12 09:56:37 +0200

    Update

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import NavbarAuth from 'components/NavbarAuth/NavbarAuth';
 import { Suspense } from 'react';
 
 const Layout = () => {
   return (
     <div>
-      <NavbarAuth />
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>

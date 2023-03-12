d4e2a4c1295202857c649cf3f18350642e75acaa
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-03-12 11:12:51 +0200

    Update

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -3,11 +3,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/contacts/operations';
 import { logOut } from 'redux/auth/operations';
 import { getError, getIsLoading } from 'redux/contacts/selectors';
+import { useAuth } from 'shared/hooks/useAuth';
 
 import ContactForm from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
 import ContactFilter from 'components/ContactFilter/ContactFilter';
 import Loader from 'components/Loader/Loader';
+
+//MUI
+import IconButton from '@mui/material/IconButton';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 
 import styles from './Phonebook.module.scss';
 
@@ -15,6 +21,7 @@ const Phonebook = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector(getIsLoading);
   const error = useSelector(getError);
+  const { user } = useAuth();
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -25,9 +32,15 @@ const Phonebook = () => {
       <div className={styles.containerBcg}>
         <div className={styles.contactBook}>
           <h1 className={styles.title}>Phonebook</h1>
-          <button type="button" onClick={() => dispatch(logOut())}>
-            Logout
-          </button>
+          <div className={styles.user}>
+            <p className={styles.user}>
+              <PersonOutlineIcon />
+              {user.name}
+            </p>
+            <IconButton aria-label="delete" onClick={() => dispatch(logOut())}>
+              <LogoutIcon sx={{ fill: '#dd2c00' }} />
+            </IconButton>
+          </div>
           <ContactForm />
           <h2 className={styles.subTitle}>Contacts</h2>
           <ContactFilter />
